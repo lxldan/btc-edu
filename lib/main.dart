ae25fb32b7d24cfe8657274898d8afe3c20da979
Alexander Danilin <danilinlx@icloud.com>
2025-03-09 16:23:13 +0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title)
       ),
+      
       body: Center(
         child: RiveAnimation.asset(
            'assets/network.riv'
