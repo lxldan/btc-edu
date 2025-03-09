56ff9593be02a0fcc5ca417159589aaca6992a21
Alexander Danilin <danilinlx@icloud.com>
2025-03-09 16:16:01 +0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: RiveAnimation.asset(
-          'rive/network.riv'
+           'assets/network.riv'
         )
       )
     );
