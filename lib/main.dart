9423ef3f3680b39b46b65e3e601df5b99218d280
Alexander Danilin <danilinlx@icloud.com>
2025-03-09 21:12:38 +0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: RiveAnimation.network(
-          'https://wkoghhjwpryqvljnbshj.supabase.co/storage/v1/object/public/test//untitled.riv'
+          'assets/rive/network.riv',
         )
       ),
       // floatingActionButton: FloatingActionButton(
