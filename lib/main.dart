897f99ed2fb28189616f96a4771ed4949028b8d4
Alex Danilin <danilinlx@icloud.com>
2025-03-12 12:28:56 +0500

    TG auth info text

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
 import 'package:telegram_web_app/telegram_web_app.dart';
 
 void main() {
@@ -11,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,26 +36,19 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            onPressed: () {
-              if (TelegramWebApp.instance.isSupported) {
-                print(TelegramWebApp.instance.initData.toString());
-                print(TelegramWebApp.instance.version);
-              } else {
-                print('It is not launched as TG mini app');
-              }
-            },
-            icon: Icon(Icons.info),
-          ),
-        ],
+        title: Text(widget.title)
       ),
-      body: Center(
-        child: RiveAnimation.network(
-          'https://wkoghhjwpryqvljnbshj.supabase.co/storage/v1/object/public/test//untitled.riv',
-        ),
-      ),
+      body: Stack(
+        children: [
+          if (TelegramWebApp.instance.isSupported)...[
+            Text('Authorized by telegram'),
+            Text('Telegram version: ${TelegramWebApp.instance.version}'),
+            Text('Telegram init data: ${TelegramWebApp.instance.initData}'),
+          ] else ...[
+            Text('No authorized'),
+          ]
+        ]
+      )
     );
   }
 }
